c29e65d9f769a917b18f93f8b1c01a82be212fbe
krendus <xaiyax01@gmail.com>
2023-11-03 16:49:43 +0100

    added admin controller

---
+++ src/controllers/admin.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export const login = (req: Request, res: Response) => {
+
+}
+export const getAllSchools = (req: Request, res: Response) => {
+
+}
+export const getSingleSchool = (req: Request, res: Response) => {
+
+}
+export const validateSchool = (req: Request, res: Response) => {
+
+}
+export const deleteSchool = (req: Request, res: Response) => {
+
+}
--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -1,18 +1,19 @@
 import { Router } from "express";
 import { verifyAdmin } from "../middlewares/auth.middleware";
+import { deleteSchool, getAllSchools, getSingleSchool, login, validateSchool } from "../controllers/admin.controller";
 
 const adminRouter = Router();
 const authAdminRouter = Router();
 
 
 // Unauthenticated route
-adminRouter.post("/login");
+adminRouter.post("/login", login);
 
 // Authenticated routes
-authAdminRouter.get("/school");
-authAdminRouter.get("/school/:id");
-authAdminRouter.post("/school/:id/validate");
-authAdminRouter.delete("/school/:id");
+authAdminRouter.get("/schools", getAllSchools);
+authAdminRouter.get("/schools/:id", getSingleSchool);
+authAdminRouter.post("/schools/:id/validate", validateSchool);
+authAdminRouter.delete("/schools/:id", deleteSchool);
 
 // Including middleware to auth routes
 adminRouter.use("/", verifyAdmin, authAdminRouter);
