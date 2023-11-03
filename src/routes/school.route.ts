bf2ff13ef74a01fb6d473ec172070c4ba9f3e73b
krendus <xaiyax01@gmail.com>
2023-11-03 14:25:49 +0100

    fixes

--- src/routes/school.route.ts
+++ src/routes/school.route.ts
@@ -6,7 +6,7 @@ const authSchoolRouter = Router();
 
 // Authenticated routes
 authSchoolRouter.patch("/:id");
-authSchoolRouter.get("/:id")
+authSchoolRouter.get("/:id");
 authSchoolRouter.post("/students");
 authSchoolRouter.get("/students");
 authSchoolRouter.get("/students/:id");
@@ -15,10 +15,17 @@ authSchoolRouter.delete("/students/:id");
 authSchoolRouter.post("/exams");
 authSchoolRouter.get("/exams");
 authSchoolRouter.get("/exams/:id");
+authSchoolRouter.get("/exams/:id/download");
 authSchoolRouter.patch("/exams/:id");
 authSchoolRouter.delete("/exams/:id");
 authSchoolRouter.post("/exams/:id/start");
 authSchoolRouter.post("/exams/:id/end");
+authSchoolRouter.post("/exams/questions");
+authSchoolRouter.get("/exams/questions/all/:exam_id");
+authSchoolRouter.get("/exams/questions/all");
+authSchoolRouter.get("/exams/questions/:id");
+authSchoolRouter.patch("/exams/questions/:id");
+authSchoolRouter.delete("/exams/questions/:id");
 
 // Including middleware to auth routes
 schoolRouter.use("/", verifySchool, authSchoolRouter);
